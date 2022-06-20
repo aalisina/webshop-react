0582d0dcbd662f861791f36a3a068d5ce5e00e90
sinazay123 <sinazay123@gmail.com>
2022-06-20 16:34:38 -0500

    Installed react-router-dom and added the routes for the components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import Home from "./Components/Home/Home";
 import Footer from "./Components/Footer/Footer";
 import Details from "./Components/Details.jsx/Details";
 import Checkout from "./Components/Checkout/Checkout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <Home />
-      <Details />
-      <Checkout />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="details" element={<Details />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
