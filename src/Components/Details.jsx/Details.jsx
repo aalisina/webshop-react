39f1cdf61b130b8cb6ef3e430d28162be405ff58
sinazay123 <sinazay123@gmail.com>
2022-06-20 16:17:19 -0500

    Added image and corrected path

--- src/Components/Details.jsx/Details.jsx
+++ src/Components/Details.jsx/Details.jsx
@@ -9,7 +9,7 @@ function Details() {
           <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
             <div className="col-10 col-sm-8 col-lg-6">
               <img
-                src="bootstrap-themes.png"
+                src="/assets/bootstrap-themes.png"
                 className="d-block mx-lg-auto img-fluid"
                 alt="Bootstrap Themes"
                 width={700}
