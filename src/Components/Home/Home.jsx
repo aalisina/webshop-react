fd4067e1e514d41ee91bb0b066896cd23cce4a27
sinazay123 <sinazay123@gmail.com>
2022-06-20 23:26:12 -0500

    Added states for managing category and size TODO: need to figure out how to filter when both states are "active"

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const products = [
   {
@@ -108,7 +108,7 @@ const products = [
   },
   {
     id: 8,
-    category: "shirts",
+    category: "shoes",
     image: "fitness8.jpg",
     name: "Shoes Extreme Comfort",
     price: 155.95,
@@ -124,21 +124,42 @@ const products = [
 ];
 
 function Home() {
+  const [category, setCategory] = useState("");
+  const [size, setSize] = useState("");
+
+  const filteredProductsByCategory = category
+    ? products.filter((p) => p.category === category)
+    : products;
+
+  const cats = products.map((p) => p.category);
+  const catUniq = [...new Set(cats)];
+  
   return (
     <>
       {/* Section*/}
       <section id="filters" className="py-5">
         <div className="container px-4 px-lg-5 mt-5">
-        <label htmlFor="size"> Filter by category:</label>{" "}
-          <select id="category">
+          <label htmlFor="category"> Filter by category:</label>{" "}
+          <select
+            id="category"
+            value={category}
+            onChange={(event) => {
+              setCategory(event.target.value);
+            }}
+          >
             <option value="">All categories</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
+            {catUniq.map((cat)=> (<option value={cat}>{cat}</option>))}
           </select>
           <br />
           <br />
           <label htmlFor="size">Filter by size:</label>{" "}
-          <select id="size">
+          <select
+            id="size"
+            value={size}
+            onChange={(event) => {
+              setSize(event.target.value);
+            }}
+          >
             <option value="">All sizes</option>
             <option value="8">8</option>
             <option value="9">9</option>
@@ -148,56 +169,53 @@ function Home() {
       <section className="py-5">
         <div className="container px-4 px-lg-5 mt-5">
           <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-
-            {products.map((product)=> (
-                          <div className="col mb-5">
-                          <div className="card h-100">
-                            {/* Sale badge*/}
-                            <div
-                              className="badge bg-dark text-white position-absolute"
-                              style={{ top: "0.5rem", right: "0.5rem" }}
-                            >
-                              Sale
-                            </div>
-                            {/* Product image*/}
-                            <img
-                              className="card-img-top"
-                              src={`/assets/images/${product.image}`}
-                              alt="..."
-                            />
-                            {/* Product details*/}
-                            <div className="card-body p-4">
-                              <div className="text-center">
-                                {/* Product name*/}
-                                <h5 className="fw-bolder">{product.name}</h5>
-                                {/* Product reviews*/}
-                                <div className="d-flex justify-content-center small text-warning mb-2">
-                                  <div className="bi-star-fill" />
-                                  <div className="bi-star-fill" />
-                                  <div className="bi-star-fill" />
-                                  <div className="bi-star-fill" />
-                                  <div className="bi-star-fill" />
-                                </div>
-                                {/* Product price*/}
-                                <span className="text-muted text-decoration-line-through">
-                                ${Math.round(product.price * 1.3) } {" "}
-                                </span>
-                                $ {product.price}
-                              </div>
-                            </div>
-                            {/* Product actions*/}
-                            <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                              <div className="text-center">
-                                <a className="btn btn-outline-dark mt-auto" href="#">
-                                  Add to cart
-                                </a>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
+            {products.map((product) => (
+              <div className="col mb-5">
+                <div className="card h-100">
+                  {/* Sale badge*/}
+                  <div
+                    className="badge bg-dark text-white position-absolute"
+                    style={{ top: "0.5rem", right: "0.5rem" }}
+                  >
+                    Sale
+                  </div>
+                  {/* Product image*/}
+                  <img
+                    className="card-img-top"
+                    src={`/assets/images/${product.image}`}
+                    alt="..."
+                  />
+                  {/* Product details*/}
+                  <div className="card-body p-4">
+                    <div className="text-center">
+                      {/* Product name*/}
+                      <h5 className="fw-bolder">{product.name}</h5>
+                      {/* Product reviews*/}
+                      <div className="d-flex justify-content-center small text-warning mb-2">
+                        <div className="bi-star-fill" />
+                        <div className="bi-star-fill" />
+                        <div className="bi-star-fill" />
+                        <div className="bi-star-fill" />
+                        <div className="bi-star-fill" />
+                      </div>
+                      {/* Product price*/}
+                      <span className="text-muted text-decoration-line-through">
+                        ${Math.round(product.price * 1.3)}{" "}
+                      </span>
+                      $ {product.price}
+                    </div>
+                  </div>
+                  {/* Product actions*/}
+                  <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                    <div className="text-center">
+                      <a className="btn btn-outline-dark mt-auto" href="#">
+                        Add to cart
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
             ))}
-
-            
           </div>
         </div>
       </section>
