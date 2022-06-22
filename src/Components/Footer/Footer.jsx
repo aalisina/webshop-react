c0f1c94779db16fae82af3087e54cdbdee706194
sinazay123 <sinazay123@gmail.com>
2022-06-22 12:21:41 -0500

    Added Appmakelaar text in the footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -29,11 +29,8 @@ function Footer() {
               href="/"
               className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
             >
-              <svg className="bi" width={30} height={24}>
-                <use xlinkHref="#bootstrap" />
-              </svg>
             </a>
-            <span className="mb-3 mb-md-0 text-muted">© 2022 Company, Inc</span>
+            <span className="mb-3 mb-md-0 text-muted">©  {new Date().getFullYear()} Appmakelaar.nl</span>
           </div>
           <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
             <li className="ms-3">
