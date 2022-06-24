7dc56ff76ed43c079fc8e09424ae646478760a2f
sinazay123 <sinazay123@gmail.com>
2022-06-24 11:10:34 -0500

    Added dynamic year and name of Appmakelaar

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -28,9 +28,10 @@ function Footer() {
             <a
               href="/"
               className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
-            >
-            </a>
-            <span className="mb-3 mb-md-0 text-muted">©  {new Date().getFullYear()} Appmakelaar.nl</span>
+            ></a>
+            <span className="mb-3 mb-md-0 text-muted">
+              © {new Date().getFullYear()} Appmakelaar.nl
+            </span>
           </div>
           <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
             <li className="ms-3">
