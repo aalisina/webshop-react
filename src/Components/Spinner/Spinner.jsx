5b093fb9754aff72466771a10b0572ec3e41bb44
sinazay123 <sinazay123@gmail.com>
2022-06-24 12:27:28 -0500

    Centered the spinner by wrapping in a new div and adding some css

--- src/Components/Spinner/Spinner.jsx
+++ src/Components/Spinner/Spinner.jsx
@@ -3,8 +3,10 @@ import "./spinner.css";
 
 function Spinner() {
   return (
-    <div className="spinner-container">
-      <div className="loading-spinner"></div>
+    <div className="center">
+      <div className="spinner-container">
+        <div className="loading-spinner"></div>
+      </div>
     </div>
   );
 }
