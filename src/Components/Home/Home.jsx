8347ba2a7608b83bb0384d984caf747bd2401253
sinazay123 <sinazay123@gmail.com>
2022-06-26 20:06:42 -0500

    Added links around the products to get the user to the product details page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,6 +3,7 @@ import Spinner from "../../Components/Spinner/Spinner";
 import useFetch from "../../Services/useFetch";
 import { useParams } from "react-router-dom";
 import PageNotFound from "../PageNotFound/PageNotFound";
+import { Link } from "react-router-dom";
 
 function Home() {
   const [category, setCategory] = useState("");
@@ -84,49 +85,51 @@ function Home() {
           <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
             {filteredProductsBySizeAndCategory.map((product) => (
               <div className="col mb-5">
-                <div className="card h-100">
-                  {/* Sale badge*/}
-                  <div
-                    className="badge bg-dark text-white position-absolute"
-                    style={{ top: "0.5rem", right: "0.5rem" }}
-                  >
-                    Sale
-                  </div>
-                  {/* Product image*/}
-                  <img
-                    className="card-img-top"
-                    src={`/assets/images/${product.image}`}
-                    alt="..."
-                  />
-                  {/* Product details*/}
-                  <div className="card-body p-4">
-                    <div className="text-center">
-                      {/* Product name*/}
-                      <h5 className="fw-bolder">{product.name}</h5>
-                      {/* Product reviews*/}
-                      <div className="d-flex justify-content-center small text-warning mb-2">
-                        <div className="bi-star-fill" />
-                        <div className="bi-star-fill" />
-                        <div className="bi-star-fill" />
-                        <div className="bi-star-fill" />
-                        <div className="bi-star-fill" />
+                <Link to={`/${datatype}/${product.id}`}>
+                  <div className="card h-100">
+                    {/* Sale badge*/}
+                    <div
+                      className="badge bg-dark text-white position-absolute"
+                      style={{ top: "0.5rem", right: "0.5rem" }}
+                    >
+                      Sale
+                    </div>
+                    {/* Product image*/}
+                    <img
+                      className="card-img-top"
+                      src={`/assets/images/${product.image}`}
+                      alt="..."
+                    />
+                    {/* Product details*/}
+                    <div className="card-body p-4">
+                      <div className="text-center">
+                        {/* Product name*/}
+                        <h5 className="fw-bolder">{product.name}</h5>
+                        {/* Product reviews*/}
+                        <div className="d-flex justify-content-center small text-warning mb-2">
+                          <div className="bi-star-fill" />
+                          <div className="bi-star-fill" />
+                          <div className="bi-star-fill" />
+                          <div className="bi-star-fill" />
+                          <div className="bi-star-fill" />
+                        </div>
+                        {/* Product price*/}
+                        <span className="text-muted text-decoration-line-through">
+                          ${Math.round(product.price * 1.3)}{" "}
+                        </span>
+                        $ {product.price}
                       </div>
-                      {/* Product price*/}
-                      <span className="text-muted text-decoration-line-through">
-                        ${Math.round(product.price * 1.3)}{" "}
-                      </span>
-                      $ {product.price}
+                    </div>
+                    {/* Product actions*/}
+                    <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                      <div className="text-center">
+                        <a className="btn btn-outline-dark mt-auto" href="#">
+                          Add to cart
+                        </a>
+                      </div>
                     </div>
                   </div>
-                  {/* Product actions*/}
-                  <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                    <div className="text-center">
-                      <a className="btn btn-outline-dark mt-auto" href="#">
-                        Add to cart
-                      </a>
-                    </div>
-                  </div>
-                </div>
+                </Link>
               </div>
             ))}
           </div>
