1555fba3a5ee8457845decaab4d910be9fdc5a9f
sinazay123 <sinazay123@gmail.com>
2022-06-27 16:00:18 -0500

    Added another custom hook, useFetchAll to make multiple api calls

---
+++ src/Services/useFetchAll.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+export default function useFetch(products) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const promises = products.map((p) => {
+      fetch(`${process.env.REACT_APP_API_BASE_URL}products/${p.id}`).then(
+        (res) => {
+          if (res.ok) return res.json();
+          throw res;
+        }
+      );
+    });
+
+    Promise.all(promises)
+      .then((json) => setData(json))
+      .catch((e) => {
+        console.log(e);
+        setError(e);
+      })
+      .finally(() => setLoading(false));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return { data, error, loading };
+}
