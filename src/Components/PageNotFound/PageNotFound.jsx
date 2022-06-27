397fb4f9c9b81ab47fcd7236a059d34bd67ed2f8
sinazay123 <sinazay123@gmail.com>
2022-06-26 19:13:17 -0500

    Created a page not found.

---
+++ src/Components/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,11 @@
+export default function PageNotFound() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <h1>Page not found</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
