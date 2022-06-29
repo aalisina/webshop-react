6c0020b0c12f9ebe0ddab8de6ebc277540e4da12
sinazay123 <sinazay123@gmail.com>
2022-06-28 23:04:05 -0500

    Added firstName, lastName and address to the api endpoint shippingDetails and changed the name of the functions in orderService

--- src/Services/orderService.js
+++ src/Services/orderService.js
@@ -1,19 +1,19 @@
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
-export async function getShippingAddress(userId) {
-  const url = `${baseUrl}shippingAddress/${userId}`;
+export async function getShippingDetails(userId) {
+  const url = `${baseUrl}shippingDetails/${userId}`;
   const response = await fetch(url);
   return response.json();
 }
 
-export async function saveShippingAddress(address) {
-  const url = `${baseUrl}shippingAddress`;
+export async function saveShippingDetails(details) {
+  const url = `${baseUrl}shippingDetails`;
   const response = await fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(address),
+    body: JSON.stringify(details),
   });
   return response.json();
 }
