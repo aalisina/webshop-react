c9d59bfde99ff22ca3363d82a15840d74e3c83b8
asalizay <sinazay123@gmail.com>
2022-07-04 20:51:24 +0200

    Removed the functions for managing the cart from the App component and imported the cartReducer

--- src/App.js
+++ src/App.js
@@ -1,51 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 import Navigation from "./Components/Navigation/Navigation";
 import Home from "./Components/Home/Home";
 import Footer from "./Components/Footer/Footer";
 import Details from "./Components/Details/Details";
 import Checkout from "./Components/Checkout/Checkout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import cartReducer from "./Reducers/cartReducer"
+
+let initialCart;
+
+  try {
+    initialCart = JSON.parse(localStorage.getItem("cart")) ?? [];
+  } catch (error) {
+    console.error("The cart could not be parsed to JSON.");
+    initialCart = [];
+  }
 
 function App() {
-  const [cart, setCart] = useState(() => {
-    try {
-      return JSON.parse(localStorage.getItem("cart")) ?? [];
-    } catch (error) {
-      console.error("The cart could not be parsed to JSON.");
-      return [];
-    }
-  });
+  const [cart, dispatch] = useReducer(cartReducer, initialCart)
 
   useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
-
-  const addToCart = (id, sku) => {
-    setCart((curState) => {
-      const itemAlreadyInCart = curState.find((item) => item.sku === sku);
-      // if(itemAlreadyInCart) itemInCart.quantity++; Doesn't work, need to treat state as immutable
-      if (itemAlreadyInCart) {
-        // Return a new array with the matching item replaced
-        return curState.map((item) =>
-          item.sku === sku ? { ...item, quantity: item.quantity + 1 } : item
-        );
-      } else {
-        // Return a new array with the new item attached to it
-        return [...curState, { id, sku, quantity: 1 }];
-      }
-    });
-  };
-
-  const updateQuantity = (sku, quantity) => {
-    setCart((curItems) => {
-      if (quantity === 0) {
-        return curItems.filter((item) => item.sku !== sku);
-      }
-      return curItems.map((item) =>
-        item.sku === sku ? { ...item, quantity } : item
-      );
-    });
-  };
-
-  const emptyCart = () => setCart([]);
 
   return (
     <div className="App">
@@ -59,15 +33,14 @@ function App() {
           <Route path="/:datatype" element={<Home />} />
           <Route
             path="/:datatype/:id"
-            element={<Details addToCart={addToCart} />}
+            element={<Details dispatch={dispatch} />}
           />
           <Route
             path="cart"
             element={
               <Checkout
                 cart={cart}
-                updateQuantity={updateQuantity}
-                emptyCart={emptyCart}
+                dispatch={dispatch}
               />
             }
           />
