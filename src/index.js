3ae4078101a9329e18934c1f2de5c0f01f80e28d
asalizay <sinazay123@gmail.com>
2022-07-22 16:08:05 +0200

    Wrapped the CartProvider component arount the App

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import ErrorBoundary from "./ErrorBoundary";
+import { CartProvider } from "./Contexts/cartContext"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </ErrorBoundary>
   </React.StrictMode>
 );
